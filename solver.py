78e824f97751770fcf5969a892111bd58ace431e
frkcrc <frkcrc@gmail.com>
2023-03-15 14:40:27 +0100

    Add cli skeleton

---
+++ solver.py
@@ -0,0 +1,9 @@
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('Usage: python3 solver.py <file>')
+        exit()
+    path = sys.argv[1]
+    # TODO: Implement actual solve.
+    
